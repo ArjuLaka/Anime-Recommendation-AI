88c6164e9bc9e5e984a8220144934b9e7fe5f8ac
ArjuLaka <107326774+ArjuLaka@users.noreply.github.com>
2025-06-26 20:46:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ import matplotlib as plt
 
 # This is a test
 
+# Pandas
 df = pd.DataFrame({"A": [1, 2], "B": {3, 4}})
 print(df)
 
+# Numpy
 arr = np.array([1, 2, 3, 4, 5])
 print(arr)
+
+# Matplotlib
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
